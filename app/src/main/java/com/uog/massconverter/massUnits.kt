2ec264752122f095d3d4c6288f15ca78d8cc5a4b
Vytautasmart <vm6995c@gre.ac.uk>
2024-12-09 12:07:53 +0000

    Added custom Dropdown boxes that take input as a string, need to map the string values to integer values to convert.

---
+++ app/src/main/java/com/uog/massconverter/massUnits.kt
@@ -0,0 +1,16 @@
+package com.uog.massconverter
+
+//data class MassUnit(val name: String, val value: Int)
+
+//val massUnits = listOf(
+//MassUnit("Inch", 1),
+  //  MassUnit("Foot", 12),
+  //  MassUnit("Yard", 36),
+  //  MassUnit("mile", 63360))
+
+val massUnit  = mapOf(
+    "Inch" to 1,
+    "foot" to 12,
+    "yard" to 36,
+    "mile" to 63360
+)
